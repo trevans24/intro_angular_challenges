bbedc114d12f252b058c7659cf472d4b4f65fab2
tgaff <tgaff@alumni.nd.edu>
2016-04-12 15:44:17 -0700

    add semi-colons

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,7 +1,7 @@
 console.log('challenge #2 -- app.js loaded!');
 
 // var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController)
+// app.controller("WelcomeController", WelcomeController);
 
 // function WelcomeController(){
 //   this.full_name = "YOUR NAME"
