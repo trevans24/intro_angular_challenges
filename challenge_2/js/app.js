54c7b61e87e3a5a5061aa32eecf06a0c1c466280
nathanallen <thenathanator@gmail.com>
2016-04-13 10:54:36 -0700

    solution c2e2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -2,6 +2,7 @@ console.log('challenge #2 -- app.js loaded!');
 
 var app = angular.module("appTwo", []);
 app.controller("WelcomeController", WelcomeController);
+app.controller("CohortController", CohortController);
 
 function WelcomeController(){
   this.full_name = "John Doe";
@@ -10,5 +11,17 @@ function WelcomeController(){
   this.state = "CA"
   this.shout = function(str){
     return str.toUpperCase() + "!";
+  }
+}
+
+function CohortController(){
+  this.class_name = "WDI";
+  this.enrolled_students = ["Sally", "John", "Jane", "Sean"];
+  this.start_date = "4/01/2020";
+  this.end_date = "7/01/2020";
+  this.daysRemaining = function(){
+    var ms_left = Date.parse(this.end_date) - Date.now();
+    var days_left = Math.floor( ms_left/1000/24/60/60 );
+    return days_left;
   }
 }
