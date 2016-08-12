5e1e696cf8dee59e4a9c1d932731273ac48f4256
J.P. Barela <barela@jpbarela.com>
2016-08-12 09:03:46 -0600

    Add back js files

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,3 +1,8 @@
 console.log('challenge #2 -- app.js loaded!');
 
+// var app = angular.module("appTwo", []);
+// app.controller("WelcomeController", WelcomeController);
 
+// function WelcomeController(){
+//   this.full_name = "YOUR NAME"
+// }
