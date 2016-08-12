7c7f312c29c0d560f1c407d9c0adb7d5f76d9c1d
J.P. Barela <barela@jpbarela.com>
2016-08-12 07:51:37 -0600

    Update Angular Version

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,3 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
