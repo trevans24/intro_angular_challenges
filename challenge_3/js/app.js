3bd6453bc2576caa815d4a541137065a28dfd9bb
trevans24 <trevans24@gmail.com>
2017-04-26 08:46:39 -0600

    finished challenge 3

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,28 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController($scope){
+  // this.name = "Jon Doe"; // default name
+  // this.height = 5.6; // default height in feet
+  // $scope.info =[]
+}
+
+app.controller('loanController', loanController);
+
+function loanController($scope){
+	this.amount = 0;
+    this.apr = 0;
+    this.month = function(n){
+      	var yearly_interest = (this.amount * (this.apr/100));
+      	var monthly_interst = (yearly_interest / 12);
+   		var accrued_interest = (n * monthly_interst || 0);
+   		var total_balance = (this.amount + accrued_interest);
+    		return {
+    			accrued_interest,
+    			total_balance
+    };
+	};
+}
+	
